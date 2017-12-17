136ea989dc99171b23eeaa8853e73bd93e51188d
rageMule <30770412+ChowXue@users.noreply.github.com>
2017-12-17 22:34:05 +0800

    Add files via upload

---
+++ src/setting.py
@@ -0,0 +1,6 @@
+## Your Telegram Channel Name ##
+channel_name = 'rage_mule'
+## Telegram Access Token ##
+telegram_token = '497908699:AAGJxIx0QSc_HE2-DZkRmLTNfw9f38OaHxU'
+## Twitter User Name to get Timeline ##
+user_name = 'rage_mule'
